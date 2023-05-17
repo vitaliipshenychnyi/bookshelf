86abaf98675f1b23ee946902c12935daf1fe5366
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-05-17 12:33:22 +0300

    Update shopping.js

--- src/shopping.js
+++ src/shopping.js
@@ -3,6 +3,9 @@ import './js/support';
 import './js/render-photo-card-footer';
 import './js/shopingList';
 
+// !!! функціонал відкриття вікна регістрації
+import './js/authentication-service/auth-formdata';
+
 import refs from './js/refs';
 import { changeTheme } from './js/togle-theme';
 
